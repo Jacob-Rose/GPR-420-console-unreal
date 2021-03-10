801564c636fed54be265ca1c9748c0a19f38a71c
Jacob Rose <jacob.rose@mymail.champlain.edu>
2021-03-09 19:53:53 -0500

    added second call for http to get the actual forecast

--- SimpleFPSTemplate-master 4.26 - 2/Source/FPSGame/Private/WeatherHttpActor.cpp
+++ SimpleFPSTemplate-master 4.26 - 2/Source/FPSGame/Private/WeatherHttpActor.cpp
@@ -39,6 +39,7 @@ void AWeatherHttpActor::WeatherRequestRecieved(FHttpRequestPtr Request, FHttpRes
 {
 	if (bWasSuccessful)
 	{
+		//https://api.weather.gov/points/44.4793,-73.1972 example
 		TSharedPtr<FJsonObject> JsonObject;
 		TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
 
@@ -46,12 +47,32 @@ void AWeatherHttpActor::WeatherRequestRecieved(FHttpRequestPtr Request, FHttpRes
 		{
 			TSharedPtr<FJsonObject> val = JsonObject->GetObjectField("properties");
 			FString forecastLink = val->GetStringField("forecast");
-
+			TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = HttpModule->CreateRequest();
+			HttpRequest->SetVerb("GET");
+			HttpRequest->SetHeader("Content-Type", TEXT("application/json"));
+			HttpRequest->SetURL(forecastLink);
+			HttpRequest->OnProcessRequestComplete().BindUObject(this, &AWeatherHttpActor::WeatherForcastRecieved);
+			HttpRequest->ProcessRequest();
 		}
 	}
 }
 
 void AWeatherHttpActor::WeatherForcastRecieved(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 {
+	if (bWasSuccessful)
+	{
+		TSharedPtr<FJsonObject> JsonObject;
+		TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+
+		if (FJsonSerializer::Deserialize(JsonReader, JsonObject))
+		{
+			TSharedPtr<FJsonObject> properties = JsonObject->GetObjectField("properties");
+			TArray<TSharedPtr<FJsonValue>> periods = properties->GetArrayField("periods");
+			//https://api.weather.gov/gridpoints/BTV/88,56/forecast example
+			//Actual useful data example
+			FString shortForecast = periods[0]->AsObject()->GetStringField("shortForecast"); //Partly Cloudy, Cloudy, Sunny, Rain
+
+		}
+	}
 }
 
